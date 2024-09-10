cc3f269a8f1942d66625e5e9fa0293d3d6ce701a
Sajjad-Frhngi <sajjadfarhangi915@gmail.com>
2024-09-11 01:20:33 +0330

    Sajjad: Huge Edit: add boxes, glassy navbar, and some js codes...

--- resources/js/home.js
+++ resources/js/home.js
@@ -2,16 +2,67 @@ const slider = document.getElementById("slider");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
 
+const autoScrollInterval = 800;
+const scrollAmount = 275;
+const scrollAmountBack = 1000;
+
+function autoScroll() {
+  slider.scrollBy({
+    left: scrollAmountBack,
+    behavior: "smooth",
+  });
+
+  if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+    slider.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+}
+
+let autoScrollIntervalId = setInterval(autoScroll, autoScrollInterval);
+
+function stopAutoScroll() {
+  clearInterval(autoScrollIntervalId);
+}
+
+function startAutoScroll() {
+  autoScrollIntervalId = setInterval(autoScroll, autoScrollInterval);
+}
+
+slider.addEventListener("mouseover", stopAutoScroll);
+slider.addEventListener("mouseout", startAutoScroll);
+
+nextBtn.addEventListener("mouseover", stopAutoScroll);
+nextBtn.addEventListener("mouseout", startAutoScroll);
+
+prevBtn.addEventListener("mouseover", stopAutoScroll);
+prevBtn.addEventListener("mouseout", startAutoScroll);
+
 nextBtn.addEventListener("click", () => {
   slider.scrollBy({
-    left: -300, // مقدار پیکسل که اسلایدر به سمت راست حرکت می‌کند
+    left: -scrollAmount,
     behavior: "smooth",
   });
 });
 
 prevBtn.addEventListener("click", () => {
   slider.scrollBy({
-    left: 300, // مقدار پیکسل که اسلایدر به سمت چپ حرکت می‌کند
+    left: scrollAmount,
     behavior: "smooth",
   });
 });
+
+window.onscroll = function () {
+  var navbar = document.querySelector(".navbar");
+  var sticky = navbar.offsetTop;
+  if (window.pageYOffset > sticky) {
+    navbar.style.background = "#5c5c5c55";
+    navbar.style.position = "fixed";
+    navbar.style.top = "0";
+  } else {
+    navbar.style.position = "relative";
+    navbar.style.top = "auto";
+    navbar.style.background = "#fff";
+  }
+};
